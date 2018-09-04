71e7e4d5f0df482e554024a9e3dc1a0b6f532278
Peter Lee <peterlee@peters-macbook-pro-2.local>
2018-09-04 10:14:28 -0400

    2.5 Completed

--- You are awesome!/ViewController.swift
+++ You are awesome!/ViewController.swift
@@ -20,7 +20,12 @@ class ViewController: UIViewController {
 
     @IBAction func showMessagePressed(_ sender: UIButton) {
         messageLabel.text = "You Are Awesome!"
+        messageLabel.textColor = UIColor.red
     }
     
+    @IBAction func showAnotherMessagePressed(_ sender: UIButton) {
+        messageLabel.text = "You Are Great!"
+        messageLabel.textColor = UIColor.blue
+    }
 }
 
